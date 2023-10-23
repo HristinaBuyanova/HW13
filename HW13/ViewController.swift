22eac3d82dd73adf0472044062aa5afb5579238f
HristinaBuyanova <hristina-a@mail.ru>
2023-10-24 01:47:21 +0300

    Initial Commit

---
+++ HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13
+//
+//  Created by Христина Буянова on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
