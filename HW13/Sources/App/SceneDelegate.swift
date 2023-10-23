8c2425f68298eec0a7ab7ed2d6e69e73a1ba54a7
HristinaBuyanova <hristina-a@mail.ru>
2023-10-24 01:59:20 +0300

    Project preparation

---
+++ HW13/Sources/App/SceneDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  SceneDelegate.swift
+//  HW13
+//
+//  Created by Христина Буянова on 24.10.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+                       window = UIWindow(windowScene: windowScene)
+                       let viewController = ViewController()
+                       window?.rootViewController = viewController
+                       window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+     
+    }
+
+
+}
+
